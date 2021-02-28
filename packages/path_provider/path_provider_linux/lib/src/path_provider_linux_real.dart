9d59099dae33217e9cb608ce82b5877cfb838d5a
Robert Ancell <robert.ancell@canonical.com>
2021-03-01 11:39:26 +1300

    Make typedefs private

--- packages/path_provider/path_provider_linux/lib/src/path_provider_linux_real.dart
+++ packages/path_provider/path_provider_linux/lib/src/path_provider_linux_real.dart
@@ -11,12 +11,12 @@ import 'package:path/path.dart' as path;
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 
 // GApplication* g_application_get_default();
-typedef g_application_get_default_c = IntPtr Function();
-typedef g_application_get_default_dart = int Function();
+typedef _g_application_get_default_c = IntPtr Function();
+typedef _g_application_get_default_dart = int Function();
 
 // const gchar* g_application_get_application_id(GApplication* application);
-typedef g_application_get_application_id_c = Pointer<Utf8> Function(IntPtr);
-typedef g_application_get_application_id_dart = Pointer<Utf8> Function(int);
+typedef _g_application_get_application_id_c = Pointer<Utf8> Function(IntPtr);
+typedef _g_application_get_application_id_dart = Pointer<Utf8> Function(int);
 
 /// The linux implementation of [PathProviderPlatform]
 ///
@@ -40,17 +40,17 @@ class PathProviderLinux extends PathProviderPlatform {
     } on ArgumentError {
       return null;
     }
-    var g_application_get_default = gio.lookupFunction<
-        g_application_get_default_c,
-        g_application_get_default_dart>('g_application_get_default');
-    var app = g_application_get_default();
+    var _g_application_get_default = gio.lookupFunction<
+        _g_application_get_default_c,
+        _g_application_get_default_dart>('g_application_get_default');
+    var app = _g_application_get_default();
     if (app == 0) return null;
 
-    var g_application_get_application_id = gio.lookupFunction<
-            g_application_get_application_id_c,
-            g_application_get_application_id_dart>(
+    var _g_application_get_application_id = gio.lookupFunction<
+            _g_application_get_application_id_c,
+            _g_application_get_application_id_dart>(
         'g_application_get_application_id');
-    var app_id = g_application_get_application_id(app);
+    var app_id = _g_application_get_application_id(app);
     if (app_id == null) return null;
 
     return app_id.toDartString();
